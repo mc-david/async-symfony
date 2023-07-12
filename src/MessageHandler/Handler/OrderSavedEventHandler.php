28f71df705f37bf45010bdfd941cf04ae01112de
David Gerlich <david.gerlich@jobrouter.com>
2023-07-12 23:20:34 +0200

    Finish last video

--- src/MessageHandler/Handler/OrderSavedEventHandler.php
+++ src/MessageHandler/Handler/OrderSavedEventHandler.php
@@ -4,6 +4,7 @@ namespace App\MessageHandler\Handler;
 
 use App\Message\Event\OrderSavedEvent;
 use Mpdf\Mpdf;
+use Mpdf\Output\Destination;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Mime\Email;
@@ -18,11 +19,16 @@ class OrderSavedEventHandler
 
     public function __invoke(OrderSavedEvent $event)
     {
+        // Attempt to retrieve an order from MongoDB
+        throw new \RuntimeException('ORDER COULD NOT BE FOUND');
+
+        // 1. create a PDF contract note
         $mpdf = new Mpdf();
         $content = "<h1>Contract Note for Order {$event->getOrderId()}</h1>";
         $content .= '<p>Total: <b>$1898.75</b></p>';
+
         $mpdf->writeHtml($content);
-        $contractNotePdf = $mpdf->output('', 'S' );
+        $contractNotePdf = $mpdf->output('', Destination::STRING_RETURN);
 
         $email = (new Email())
             ->from('sales@stocksapp.com')
